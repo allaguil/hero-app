0f7bfbeff3d60a4e69c8afff67e93ccd2ac7d7e5
Allan Aguilar <alan-joc@hotmail.com>
2021-11-03 10:55:40 -0600

    docs rename

---
+++ docs/precache-manifest.7f28992b9f354796ce5ef9fab158380d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea0cd3e0b4b9e3cc1588bc8663420ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a2fc42d6c994398e4eb",
+    "url": "/static/js/2.1830b1da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1830b1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc366f445f52cf20154d",
+    "url": "/static/js/main.72718dbb.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
